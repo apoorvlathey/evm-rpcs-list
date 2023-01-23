cde631aaff76f70ac07dff8972c494684a475f58
apoorvlathey <latheyapoorv@gmail.com>
2023-01-24 03:08:57 +0530

    evm-rpcs-list v2.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 const _networksList = require("./networksList.json");
 
-interface Chains {
+const networksList = _networksList as {
   // JSON.stringify converts number keys to string
   [chainId: string]: {
     name: string;
     rpcs: string[];
   };
-}
-
-const networksList = _networksList as Chains;
+};
 
 export default networksList;
